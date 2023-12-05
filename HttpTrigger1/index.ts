005741fe8cb6787ed45fc2af062e4e172f436e09
小西達大 <konishitatsuhiro@konishitoorudainoMacBook-Pro.local>
2023-12-06 02:18:09 +0900

    add: シャットダウンハンドラ検証

--- HttpTrigger1/index.ts
+++ HttpTrigger1/index.ts
@@ -2,9 +2,24 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
+process.on('SIGTERM', async () => {
+    console.log('Function SIGTERM')
+    process.exit(0);
+});
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    // 10秒待機
-    await sleep(10000);
+    console.log('Function Start');
+
+    await sleep(30000);
+    context.log('30s...');
+    await sleep(60000);
+    context.log('90s...');
+    await sleep(60000);
+    context.log('150s...');
+    await sleep(60000);
+    context.log('210s...');
+
+    console.log('Function Finished');
 
     context.res = {
         body: "Hello, World!"
